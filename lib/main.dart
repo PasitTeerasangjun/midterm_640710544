948aa6a412f9d42c4321793ca13697947c90e488
Pasit Teerasangjun <Teerasangjun_p@silpakoen.edu>
2024-01-26 23:10:27 +0700

    Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+//import 'package:midterm_640710544/pages/midterm.dart';
 import 'package:midterm_640710544/pages/midterm.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
+ // hi
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
