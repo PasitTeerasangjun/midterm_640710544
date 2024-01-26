79c7694cd18ed7b180917dd0753280e9b698ff9e
Pasit Teerasangjun <Teerasangjun_p@silpakoen.edu>
2024-01-26 22:47:53 +0700

    first commit

---
+++ lib/pages/midterm.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class midterm extends StatelessWidget {
+  const midterm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
